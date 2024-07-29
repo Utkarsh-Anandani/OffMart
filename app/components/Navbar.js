4f5f556a28f7438eeda8c8a0aa6f27718c117e40
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2024-07-29 19:58:51 +0530

    created a new project

---
+++ app/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { CiSearch } from "react-icons/ci";
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[10vh] flex items-center bg-blue-600 px-5'>
+      <div className='text-white text-2xl font-bold flex gap-4 items-center w-[33vw]'>
+        <MdOutlineShoppingCart/> OFFMART
+      </div>
+      <div className='w-[34vw] '>
+        <button className='bg-white'><CiSearch/></button>
+        <input type="text" />
+      </div>
+      <div className='w-[33vw]'>
+        profile
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
