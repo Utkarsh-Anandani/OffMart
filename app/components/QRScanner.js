6eeb1eb11cd0ca9877284d29b4743a9b1802b462
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2024-08-01 22:11:29 +0530

    Updated QR Scanner
    QR Scanner working....

--- app/components/QRScanner.js
+++ app/components/QRScanner.js
@@ -1,39 +1,52 @@
 "use client"
-import React, { useEffect, useState } from 'react'
-import { Html5QrcodeScanner } from 'html5-qrcode'
+import React, { useEffect, useState } from 'react';
+import { Html5Qrcode } from 'html5-qrcode';
 
 const QRScanner = () => {
+  const [isEnabled, setEnabled] = useState(false);
+  const [QRData, setQRData] = useState("");
 
-    const [ScannerResult, setScannerResult] = useState("");
+  useEffect(() => {
+    const config = {fps: 5, qrbox: {height: 200, width: 200}};
+    const html5Qrcode = new Html5Qrcode("QRScanner");
 
-    useEffect(() => {
-        const scanner = new Html5QrcodeScanner('reader',{
-          qrbox : {
-            height : 250,
-            width : 250,
-          },
-          fps : 2 
-        });
+    const scannerStop = () => {
+      if(html5Qrcode && html5Qrcode.isScanning){
+        html5Qrcode
+        .stop()
+        .then(()=>console.log('Scanner stopped'))
+        .catch(()=>console.log('Scanner Error'))
+      }
+    }
 
-        const success = (result) => {
-            scanner.clear();
-            setScannerResult(result);
-        }
+    const success = (result)=>{
+      setQRData(result);
+    }
 
-        const error = (err) => {
-            console.warn(err);
-        }
+    if(isEnabled){
+      html5Qrcode.start({facingMode: "environment"}, config, success)
+    }
+    else{
+      scannerStop();
+    }
 
-        scanner.render(success, error);
+    return () => {
+      scannerStop()
+    }
+  }, [isEnabled]);
 
-      }, []);
-
-
+  
   return (
-    <div id='reader'>
-      {ScannerResult}
+    <div>
+    <div className='w-[200px] h-[200px]' id='QRScanner'>
+      
     </div>
-  )
-}
+    <button onClick={()=>setEnabled(!isEnabled)} className=''>
+        {isEnabled?"Off":"On"}
+      </button>
+      <p>{QRData}</p>
+    </div>
+  );
+};
 
-export default QRScanner
+export default QRScanner;
