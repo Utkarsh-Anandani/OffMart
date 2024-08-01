ff893806f8b1200bba90778dd584850648c7ebe5
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2024-08-02 01:24:33 +0530

    Update QRScanner.js

--- app/components/QRScanner.js
+++ app/components/QRScanner.js
@@ -7,7 +7,7 @@ const QRScanner = () => {
   const [QRData, setQRData] = useState("");
 
   useEffect(() => {
-    const config = {fps: 5, qrbox: {height: 200, width: 200}};
+    const config = {fps: 5, qrbox: {height: 400, width: 400}};
     const html5Qrcode = new Html5Qrcode("QRScanner");
 
     const scannerStop = () => {
@@ -37,11 +37,11 @@ const QRScanner = () => {
 
   
   return (
-    <div>
-    <div className='w-[200px] h-[200px]' id='QRScanner'>
-      
+    <div className='w-full h-full flex flex-col items-center justify-center pt-10 gap-5'>
+    <div className='w-[400px] h-[300px] border-2 border-blue-500 text-blue-500 font-semibold text-xl flex items-center justify-center' id='QRScanner'>
+      {!isEnabled && <p>Turn on to scan products</p>}
     </div>
-    <button onClick={()=>setEnabled(!isEnabled)} className=''>
+    <button onClick={()=>setEnabled(!isEnabled)} className='text-lg font-semibold text-white bg-blue-600 px-5 py-1 rounded-xl'>
         {isEnabled?"Off":"On"}
       </button>
       <p>{QRData}</p>
