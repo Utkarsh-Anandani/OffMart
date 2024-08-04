1f542816469c2b597bdad59195c3c2338f67dfdf
NayanshreeTripathi <nayanshree.2430@gmail.com>
2024-08-04 10:19:22 +0530

    changes2

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import ProductCard from '../components/ProductCard';
+import ProductCard from './components/ProductCard';
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
@@ -12,9 +12,12 @@ export default function Home() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('/api/product')
+    fetch('/Routes/product')
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        console.log(data); // Log the data
+        setProducts(data);
+      });
   }, []);
   return (
     <main className="w-screen min-h-screen">
@@ -25,8 +28,8 @@ export default function Home() {
       </div>
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-center">
-          {products.map((product, id) => (
-            <ProductCard key={id} product={product} />
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </div>
--- pages/api/product.js
+++
@@ -1,9 +0,0 @@
-import clientPromise from '../../lib/mongodb';
-
-export default async function handler(req, res) {
-  const client = await clientPromise;
-  const db = client.db("MyAppDatabase");
-
-  const products = await db.collection("products").find({}).toArray();
-  res.json(products);
-}
