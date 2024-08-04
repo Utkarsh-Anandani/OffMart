b3a7642236b5d132c83f81da2bcffada5906e1f1
NayanshreeTripathi <nayanshree.2430@gmail.com>
2024-08-04 21:16:10 +0530

    commit

--- app/Cart/page.js
+++ app/Cart/page.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
 
 function page() {
+  
   return (
-    <div>page</div>
+    <>
+      <Navbar/>
+    </>
   )
 }
 
--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,6 +1,10 @@
+"use client"
 import React from 'react';
 
+
 const ProductCard = ({ product }) => {
+ 
+
   return (
     <div className="max-w-sm h-[70vh] rounded hover:shadow-2xl  overflow-hidden shadow-lg m-4">
       <img className="w-full hover:scale-105 h-52" src={product.thumbnail} alt={product.title} />
@@ -11,7 +15,7 @@ const ProductCard = ({ product }) => {
         <p className="text-gray-700 text-base">Discount: {product.discountPercentage}%</p>
         <p className="text-gray-700 text-base">Rate: {product.rating}/5</p>
         <div className='mt-4'>
-          <button className='bg-blue-500 text-white font-semibold w-28 py-2 rounded-md'>Add to cart</button>
+          <button className='bg-blue-500 text-white font-semibold w-28 py-2 rounded-md' >Add to cart</button>
         </div>
       </div>
 
