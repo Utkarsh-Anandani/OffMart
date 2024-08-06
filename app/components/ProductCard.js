cac5bfa2441e6a93dd6f19019e2f943ee1e9b0d5
NayanshreeTripathi <nayanshree.2430@gmail.com>
2024-08-06 23:04:37 +0530

    designing

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,31 +1,31 @@
-"use client"
+"use client";
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { add } from '../redux/cartSlice';
-
+import { toast } from 'react-hot-toast';
 
 const ProductCard = ({ product }) => {
 
   const dispatch = useDispatch();
- 
+
   const handleAdd = (product) => {
     dispatch(add(product));
+    toast.success('Added to cart');
   }
 
   return (
-    <div className="relative max-w-sm h-[70vh] rounded hover:shadow-2xl  overflow-hidden shadow-lg m-4">
+    <div className="relative max-w-sm h-[70vh] rounded hover:shadow-2xl overflow-hidden shadow-lg m-4">
       <img className="w-full hover:scale-105 h-52" src={product.thumbnail} alt={product.title} />
-      <div className=" pl-4 py-4">
+      <div className="pl-4 py-4">
         <div className="font-bold text-xl mb-2">{product.title}</div>
         <p className="text-gray-700 text-base">Brand: {product.brand}</p>
         <p className="text-gray-700 text-base">Price: ${product.price}</p>
         <p className="text-gray-700 text-base">Discount: {product.discountPercentage}%</p>
         <p className="text-gray-700 text-base">Rate: {product.rating}/5</p>
         <div className='absolute bottom-10'>
-          <button onClick={()=>handleAdd(product)} className='bg-blue-500 text-white font-semibold w-28 py-2 rounded-md' >Add to cart</button>
+          <button onClick={() => handleAdd(product)} className='bg-blue-500 hover:scale-105 hover:shadow-sm text-white font-semibold w-28 py-2 rounded-md'>Add to cart</button>
         </div>
       </div>
-
     </div>
   );
 };
