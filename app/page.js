86c6f21aaed738c5d26b2b95f5710682846979f2
NayanshreeTripathi <nayanshree.2430@gmail.com>
2024-08-06 19:36:39 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-"use client";
-import React, { useEffect, useState } from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-=======
 "use client"
 import React, { useEffect, useState } from 'react'
->>>>>>> 5f0c5f6276d330b5f74909d418c0c0e343f0f08d
 import ProductCard from './components/ProductCard';
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import CategoryCards from "./components/CategoryCard";
-<<<<<<< HEAD
-// import Cart from "./Cart/page"
-=======
->>>>>>> 5f0c5f6276d330b5f74909d418c0c0e343f0f08d
 
 
 export default function Home() {
@@ -28,20 +18,8 @@ export default function Home() {
       });
   }, []);
   return (
-<<<<<<< HEAD
-    <main className="w-screen min-h-screen">
-      {/* <Router>
-        <div> */}
-          <Navbar />
-          {/* <Routes>
-             <Route path="/Cart" element={<Cart/>} />
-          </Routes>
-        </div>   
-      </Router> */}
-=======
     <main className="max-w-screen min-h-screen">
       <Navbar/>
->>>>>>> 5f0c5f6276d330b5f74909d418c0c0e343f0f08d
       <Banner />
       <div className="mt-20">
         <CategoryCards />
